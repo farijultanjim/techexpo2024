653c24c40e212e27e2a04fa2398b3ae44b3e47cb
Farijul Tanjim <67682096+farijultanjim@users.noreply.github.com>
2024-10-08 16:33:29 +0600

    organizer page changed

--- app/about-bite/organizer/page.jsx
+++ app/about-bite/organizer/page.jsx
@@ -1,6 +1,7 @@
 // /about-bite/organizer/page.jsx
 
 import Banner from "@/components/Banner";
+import Image from "next/image";
 import React from "react";
 
 const organizerPage = () => {
@@ -13,39 +14,34 @@ const organizerPage = () => {
       />
 
       <div className="container mx-auto py-14 px-4 flex flex-col gap-8 text-xl text-justify">
+        <div className="grid place-items-center -mt-10">
+          <div className="bg-primary p-4 rounded-lg">
+            <Image
+              src="/creative-age-logo.png"
+              width={250}
+              height={150}
+              alt="creative age"
+              className=""
+            />
+          </div>
+        </div>
         <div className="flex flex-col gap-4">
-          <h2 className="text-3xl font-bold text-gray-700">About ESSAB</h2>
+          <h2 className="text-3xl font-bold text-gray-700">
+            About Creative Age
+          </h2>
           <p>
-            Established in 2013, the Electronics Safety & Security Association
-            of Bangladesh (ESSAB), a non-profit trade association, has evolved
-            into the largest professional trade association in the Bangladesh
-            with the purpose of representing, promoting and enhancing the growth
-            and professional development of the electronics life safety,
-            security, and integrated systems industry. In cooperation with a
-            local associations, ESSAB provides government advocacy and delivers
-            timely information, professional development tools, products and
-            services that members use to grow and prosper their businesses.
-          </p>
-          <p>
-            A positive advocate for the electronics life safety, security, and
-            integrated systems industry, ESSAB works at all levels to increase
-            awareness about the value of the products and services members
-            provide. The association supports new technology and applications
-            with customized and relevant training and proactively promotes the
-            industry through consumer awareness activities supported by trade
-            and media relations programs. ESSAB also supports legislation and
-            regulations that enhance public safety.
-          </p>
-          <p>
-            Guided by strong leadership and a commitment to the advancement of
-            technology and professionalism, ESSAB is on a strong foundation that
-            includes financial stability and recognition as the only
-            professional trade association that works to address issues
-            important to the well-being of the industry.
+            Creative Age leads the charge in organizing the Bangladesh
+            International Tech Expo 2024. With a proven track record of
+            delivering world- class events, Creative Age has teamed up with
+            assiduity titans and government bodies to bring a tech exhibit
+            that’s acclimatized to inspire and elevate the tech geography in
+            Bangladesh. Working in association with mates like ASSEN, Prothom
+            Alo, and prosecution support from ADCOMM, this exhibit promises to
+            be a hallmark event, driving invention and collaboration.
           </p>
         </div>
 
-        <div>
+        {/* <div>
           <h2 className="text-3xl font-bold text-gray-700">
             Who are our members
           </h2>
@@ -54,14 +50,13 @@ const organizerPage = () => {
             business:
           </h3>
           <p>
-            Fire Detection and Protection System <br />
             CCTV Surveillance System <br />
             Access Control System <br />
             Building Management System <br />
             Public Address System <br />
             Rescue and Disaster Management
           </p>
-        </div>
+        </div> */}
       </div>
     </div>
   );
